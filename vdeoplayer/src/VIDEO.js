43b51f0e22a48bc9302b95340fa01b162778e093
HACKER <albertkanashetti@gmail.com>
2023-02-23 15:55:46 +0530

    NEWVIDEOCHANGE

--- vdeoplayer/src/VIDEO.js
+++ vdeoplayer/src/VIDEO.js
@@ -12,6 +12,8 @@ const VIDEO = () => {
     const [currentTime, setCurrentTime] = useState(0);
     const [showAd, setShowAd] = useState(true);
     const [USERIMAGE, setUSERIMAGE] = useState([]);
+    const [USERLIKE, setUSERLIKE] = useState(0);
+    const [USERDISLIKE, setUSERDISLIKE] = useState(0);
 
 
     const shouldShowAd = () => currentTime >= 10 && showAd;
@@ -44,6 +46,36 @@ const VIDEO = () => {
             })
     }
 
+    const USERLIKEFUNCTION = (videoId) => {
+        setUSERLIKE((prevLikes) => ({
+            ...prevLikes,
+            [videoId]: (prevLikes[videoId] || 0) + 1,
+        }));
+        localStorage.setItem('USERLIKE', JSON.stringify(USERLIKE));
+    };
+
+    const USERDISLIKEFUNCTION = (videoId) => {
+        setUSERDISLIKE((prevLikes) => ({
+            ...prevLikes,
+            [videoId]: (prevLikes[videoId] || 0) + 1,
+        }));
+        localStorage.setItem('USERDISLIKE', JSON.stringify(USERDISLIKE));
+    };
+
+    useEffect(() => {
+        const storedLikes = localStorage.getItem('USERLIKE');
+        if (storedLikes) {
+            setUSERLIKE(JSON.parse(storedLikes));
+        }
+    }, []);
+
+    useEffect(() => {
+        const storedLikes = localStorage.getItem('USERDISLIKE');
+        if (storedLikes) {
+            setUSERDISLIKE(JSON.parse(storedLikes));
+        }
+    }, []);
+
     useEffect(() => {
         METHOD();
         ADMETHOD();
@@ -52,75 +84,79 @@ const VIDEO = () => {
 
 
     return <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
-            <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            {/* <Link className="nav-link text-white" aria-current="page" to="/HOME">Home</Link> */}
-                        </li>
-                        {/* <li className="nav-item">
+        <body className="VIDEOBACKGROUND">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                {/* <Link className="nav-link text-white" aria-current="page" to="/HOME">Home</Link> */}
+                            </li>
+                            {/* <li className="nav-item">
                 <Link className="nav-link text-white" onClick={FUNCTION}>Chat</Link>
               </li> */}
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="/FILE">Chat</Link>
-                        </li>
-                        {/* <li className="nav-item">
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" to="/FILE">Chat</Link>
+                            </li>
+                            {/* <li className="nav-item">
                 <Link className="nav-link text-white" onClick={METHOD}>Video call</Link>
               </li> */}
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="/DEV">Developers</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="/RES">Resources</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="/UPLOAD">Upload Videos</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="form-outline">
-                    <input type="search" id="form1" className="form-control" placeholder="Search Videos" aria-label="Search" onChange={(e) => setSEARCH(e.target.value)} />
-                </div>
-            </div>
-        </nav>
-
-
-        {
-            USERVIDEOLIST.filter((value) => {
-                if (SEARCH == "") {
-                    return value;
-                }
-                else if (value.TITLE.toLowerCase().includes(SEARCH.toLowerCase())) {
-                    return value;
-                }
-
-            }).map((value, index) => (
-
-                <div class="container" style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                    <div class="video">
-                        <video src={`http://localhost:3001/${value.VIDEO}`} type="video/mp4" style={{ width: "70%" }} controls></video>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" to="/DEV">Developers</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" to="/RES">Resources</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" to="/UPLOAD">Upload Videos</Link>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="text">
-                        {
-                            USERIMAGE.map((val, i) => {
-                                return <>
-                                    <img className="user-img" src={`http://localhost:3001/${val.IMAGE}`} alt="User" />
-                                   <h4 key={i}>Uploaded By:{val.FIRSTNAME} {val.LASTNAME}</h4>
-                                </>
-                            })
-                        }
-                        <p style={{ marginRight: "100%" }}><h5>Title:</h5><h6>{value.TITLE}</h6></p>
+                    <div className="form-outline">
+                        <input type="search" id="form1" className="form-control" placeholder="Search Videos" aria-label="Search" onChange={(e) => setSEARCH(e.target.value)} />
                     </div>
                 </div>
-            ))
-        }
+            </nav>
+
+            {
+                USERVIDEOLIST.filter((value) => {
+                    if (SEARCH == "") {
+                        return value;
+                    }
+                    else if (value.TITLE.toLowerCase().includes(SEARCH.toLowerCase())) {
+                        return value;
+                    }
+
+                }).map((value, index) => (
+
+                    <div class="container" style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                        <div class="video"><br />
+                            <video src={`http://localhost:3001/${value.VIDEO}`} type="video/mp4" style={{ width: "70%", border: "5px solid black" }} controls></video>
+                            <div>
+                                <i style={{ border: '1px solid' }} className="fas fa-thumbs-up fa-border fa-1x" onClick={() => USERLIKEFUNCTION(value.id)} />   {USERLIKE[value.id] || 0} <i style={{ border: '1px solid' }} className="fas fa-thumbs-down fa-border fa-1x" onClick={() => USERDISLIKEFUNCTION(value.id)} />  {USERDISLIKE[value.id] || 0}<br />
+                            </div>
+                        </div>
+                        <div class="text">
+                            {
+                                USERIMAGE.map((val, i) => {
+                                    return <>
+                                        <img className="user-img" src={`http://localhost:3001/${val.IMAGE}`} alt="User" />
+                                        <h4 key={i}>Uploaded By:{val.FIRSTNAME} {val.LASTNAME}</h4>
+                                    </>
+                                })
+                            }
+                            <p style={{ marginRight: "100%" }}><h5>Title:</h5><h6>{value.TITLE}</h6></p>
+                        </div>
+                    </div>
+                ))
+            }
+        </body>
     </>
 }
 export default VIDEO;
